3812782be5f42090cf5f32fee3b7d6121e4ef5fe
Sudhansh <mattasudhanshu@gmail.com>
2026-07-05 22:19:03 +0530

    base url update

--- clients/client_task.py
+++ clients/client_task.py
@@ -4,7 +4,7 @@ import subprocess
 import os
 import time 
 import pwinput
-
+base_path='http://127.0.0.1:8000/'
 def clients(acc_no):
     while True:
         subprocess.run('cls'if os.name=='nt' else 'clear',shell=True)
@@ -16,23 +16,23 @@ def clients(acc_no):
             receiver_acc_no=str(input('Enter Receiver\'s Account Number : '))
             pin=pwinput.pwinput(prompt='Enter Pin : ',mask='*')
             amount=int(input('Enter Amount : '))
-            response=session.patch('http://127.0.0.1:8000/Transfer',json={'your_acc_no':acc_no,'receiver_acc_no':receiver_acc_no,'pin':pin,'credit':amount,'debit':amount})
+            response=session.patch(f'{base_path}Transfer',json={'your_acc_no':acc_no,'receiver_acc_no':receiver_acc_no,'pin':pin,'credit':amount,'debit':amount})
             print(response.json())
         elif opt=='2':
             print('--Showing Your Bank Status--')
             pin=pwinput.pwinput(prompt='Enter Pin : ',mask='*')
-            response=session.post(f'http://127.0.0.1:8000/Status',json={'acc_no':acc_no,'pin':pin})
+            response=session.post(f'{base_path}Status',json={'acc_no':acc_no,'pin':pin})
             print(response.json())
         elif opt=='3':
             print('--Thanks For Using Our banking Service--')
             pin=pwinput.pwinput(prompt='Enter Pin : ',mask='*')
-            response=session.delete(f'http://127.0.0.1:8000/DeleteUser',json={'acc_no':acc_no,'pin':pin})
+            response=session.delete(f'{base_path}DeleteUser',json={'acc_no':acc_no,'pin':pin})
             print(response.json())
             time.sleep(3)
             break
         elif opt=='4':
             pin=pwinput.pwinput(prompt='Enter Pin : ',mask='*')
-            response=session.post('http://127.0.0.1:8000/History',json={'acc_no':acc_no,'pin':pin})
+            response=session.post(f'{base_path}History',json={'acc_no':acc_no,'pin':pin})
             print(response.json())
         elif opt=='5':
             print('--Logging Out--')
--- clients/login.py
+++ clients/login.py
@@ -1,15 +1,15 @@
 from .session_manager import session
 import pwinput
-
+base_path='http://127.0.0.1:8000/'
 def login():
     acc_no=str(input('Enter Account Number : '))
     password=pwinput.pwinput(prompt='Enter Password : ',mask='*')
-    response=session.post('http://127.0.0.1:8000/Login',json={'acc_no':acc_no,'pwd':password})
+    response=session.post(f'{base_path}Login',json={'acc_no':acc_no,'pwd':password})
     return response.json()
 
 def signup():
     name=str(input('Enter Name : '))
     password=pwinput.pwinput(prompt='Enter Password : ',mask='*')
     pin=pwinput.pwinput(prompt='Enter Pin : ',mask='*')
-    response=session.post('http://127.0.0.1:8000/Signup',json={'name':name,'pwd':password,'pin':pin})
+    response=session.post(f'{base_path}Signup',json={'name':name,'pwd':password,'pin':pin})
     return(response.json())
